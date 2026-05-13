915f9f35a7d10ca1331c0da7d14b7ecd698ead1f
Raquel Cabrero <cabrero.raquel@gmail.com>
2026-05-13 21:17:26 +0200

    funciones

--- get_next_line.c
+++ get_next_line.c
@@ -1,1 +1,32 @@
 #include "get_next_line.h"
+
+static void	gnl_free_and_null(char **ptr)
+{
+
+}
+
+static char	*gnl_get_line_from_saved(char *saved)
+{
+    /*
+    Recibe saved.
+    Extrae la parte hasta el primer \n y devuelve esa línea (malloc).
+    Si no hay \n, devuelve NULL.
+    */
+}
+
+static char	*gnl_read_and_join(char *saved, int fd)
+{
+    /*
+    Recibe saved[fd] y el fd.
+    Llama a read y va añadiendo datos a saved hasta que aparezca \n o EOF.
+    Devuelve saved actualizado o NULL si error.
+    */
+}
+
+char	*get_next_line(int fd)
+{
+    /*
+    Devuelve NULL si hay error o EOF sin más líneas.
+    Devuelve una línea (malloc) si todo va bien.
+    */
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,1 +1,2 @@
 #include "get_next_line.h"
+
